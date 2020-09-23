33cea4d06cc804c3ccc289340d3a83be9aa8c9c4
Kevin Glasgow <kglas001@gmail.com>
2020-09-23 21:17:15 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -124,39 +124,3 @@ def game_hash
     }
   }
 end
-
-
-#def game_hash
-#  hash = {
-#    home: {
-#      team_name: "Brooklyn Nets", 
-#      colors: ["Black", "White"],
-#     players: [
-#        player_name: [""],
-#        number: [],
-#        shoe: [],
-#        points: [],
-#        rebounds: [],
-#        assists: [],
-#        steals: [],
-#        blocks: [],
-#        slam_dunks: []
-#        ]
-#    },
-#    away: {
-#      team_name: "Charlotte Hornets",
-#      colors: ["Turquoise", "Purple"],
-#      players: [
-#        player_name: {""},
-#        number: {},
-#        shoe: {},
-#        points: {},
-#        rebounds: {},
-#        assists: {},
-#        steals: {},
-#        blocks: {},
-#        slam_dunks: {}
-#        ]
-#    }
-#  }
-#end
