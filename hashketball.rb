8f5c11c8a981de9e50fa29a604a3c6015bc821e9
Kevin Glasgow <kglas001@gmail.com>
2020-09-23 00:34:41 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -4,31 +4,67 @@ def game_hash
       team_name: "Brooklyn Nets", 
       colors: ["Black", "White"],
       players: [
-        player_name: ,
-        number: ,
-        shoe: ,
-        points: ,
-        rebounds: ,
-        assists: ,
-        steals: ,
-        blocks: ,
-        slam_dunks:
+        player_name: [""],
+        number: [],
+        shoe: [],
+        points: [],
+        rebounds: [],
+        assists: [],
+        steals: [],
+        blocks: [],
+        slam_dunks: []
         ]
     },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
       players: [
-        player_name: ,
-        number: ,
-        shoe: ,
-        points: ,
-        rebounds: ,
-        assists: ,
-        steals: ,
-        blocks: ,
-        slam_dunks:
+        player_name: [""],
+        number: [],
+        shoe: [],
+        points: [],
+        rebounds: [],
+        assists: [],
+        steals: [],
+        blocks: [],
+        slam_dunks: []
         ]
     }
   }
 end
+
+
+#def game_hash
+#  hash = {
+#    home: {
+#      team_name: "Brooklyn Nets", 
+#      colors: ["Black", "White"],
+#     players: [
+#        player_name: {""},
+#        number: {},
+#        shoe: {},
+#        points: {},
+#        rebounds: {},
+#        assists: {},
+#        steals: {},
+#        blocks: {},
+#        slam_dunks: {}
+#        ]
+#    },
+#    away: {
+#      team_name: "Charlotte Hornets",
+#      colors: ["Turquoise", "Purple"],
+#      players: [
+#        player_name: {""},
+#        number: {},
+#        shoe: {},
+#        points: {},
+#        rebounds: {},
+#        assists: {},
+#        steals: {},
+#        blocks: {},
+#        slam_dunks: {}
+#        ]
+#    }
+#  }
+#end
