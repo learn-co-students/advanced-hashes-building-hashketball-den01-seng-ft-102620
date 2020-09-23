626ddc4d8ed16a79d3bc41c9a7c1904175c6651d
Kevin Glasgow <kglas001@gmail.com>
2020-09-23 21:00:33 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -4,31 +4,76 @@ def game_hash
       team_name: "Brooklyn Nets", 
       colors: ["Black", "White"],
       players: [
-        player_name: [""],
-        number: [],
-        shoe: [],
-        points: [],
-        rebounds: [],
-        assists: [],
-        steals: [],
-        blocks: [],
-        slam_dunks: []
-        ]
+        "Alan Anderson" => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1 
+        },
+        "Reggie Evans" => {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        "Brook Lopez" => {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        "Mason Plumlee" => {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 11,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        "Jason Terry" => {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      ]
     },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
       players: [
-        player_name: [""],
-        number: [],
-        shoe: [],
-        points: [],
-        rebounds: [],
-        assists: [],
-        steals: [],
-        blocks: [],
-        slam_dunks: []
-        ]
+        "Jeff Adrien" => {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        }
+        "Bismack Biyombo" => {
+          
+        }
+      ]
     }
   }
 end
@@ -40,15 +85,15 @@ end
 #      team_name: "Brooklyn Nets", 
 #      colors: ["Black", "White"],
 #     players: [
-#        player_name: {""},
-#        number: {},
-#        shoe: {},
-#        points: {},
-#        rebounds: {},
-#        assists: {},
-#        steals: {},
-#        blocks: {},
-#        slam_dunks: {}
+#        player_name: [""],
+#        number: [],
+#        shoe: [],
+#        points: [],
+#        rebounds: [],
+#        assists: [],
+#        steals: [],
+#        blocks: [],
+#        slam_dunks: []
 #        ]
 #    },
 #    away: {
