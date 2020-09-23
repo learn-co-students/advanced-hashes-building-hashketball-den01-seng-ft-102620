ac454ce11a5dff875ac5acb67881cce01a1b8279
Kevin Glasgow <kglas001@gmail.com>
2020-09-23 00:15:19 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,16 @@
-# Write your code here!
+def game_hash
+  hash = {
+    home: {
+      team_name: "Brooklyn Nets", 
+      colors: ["Black", "White"],
+      players: [
+        ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        ]
+    }
+  }
+end
